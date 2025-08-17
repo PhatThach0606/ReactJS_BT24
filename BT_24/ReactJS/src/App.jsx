48e473af54ff865a0b17e46d06a45b542a18298e
ThachThạch Hoàng Thành Phát <thanhphat127c@gmail.com>
2025-08-17 11:26:11 +0700

    Nộp bài tập

---
+++ BT_24/ReactJS/src/App.jsx
@@ -0,0 +1,13 @@
+import Heroic from './Unit24/index'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <Heroic/>
+    </>
+  )
+}
+
+export default App
---
+++ BT_24/ReactJS/src/Unit24/index.jsx
@@ -0,0 +1,15 @@
+import Header from "./header"
+import Carousel from "./carousel"
+import Listitem from "./listItem"
+import Footer from "./footer"
+function Heroic() {
+    return (
+       <div>
+        <Header/>
+        <Carousel/>
+        <Listitem/>
+        <Footer/>
+       </div>
+    )
+}
+export default Heroic
